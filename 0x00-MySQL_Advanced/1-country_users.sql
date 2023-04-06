5401fda291db28cb6addfc26a151bcabd65c966c
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-06 13:59:48 +0000

    Created a sql script name 1-country_users that creates a table with fields id and email unique and country is a enum

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- creates a table users with columns (id, email, name, country) and unique columns: id, email
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
