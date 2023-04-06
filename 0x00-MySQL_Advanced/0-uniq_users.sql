83acc396c94f4fce2cbb47c8a1d06c184786fab6
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-06 13:24:08 +0000

    Created a sql script that creates a table with all fields unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- creates a table user with unique columns with names: id, email and name
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE,
+    name VARCHAR(255) UNIQUE
+);
