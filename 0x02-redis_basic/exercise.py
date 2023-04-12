fde0ae74b8820fd4657882ad45431842a1353327
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-12 16:47:04 +0000

    Added three methods 'get_str' convert btye to string 'get_int' convert to integer and 'get' convert bytes receive from redis.get to its rightful type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ALX SE Backend Redis Module."""
 import redis
-from typing import Union
+from typing import Union, Callable, Any
 import uuid
 
 
@@ -20,3 +20,27 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn: Union[Callable, None]):
+        """Return the value of a key in its rightful type."""
+        value: Any = self._redis.get(key)
+
+        if value is None:
+            return value
+        if fn is None:
+            return value
+        if fn is int:
+            value = self.get_int(value)
+        elif fn is str:
+            value = self.get_str(value)
+        else:
+            value = fn(value)
+        return value
+
+    def get_str(self, value: bytes) -> str:
+        """Return the string version of a btye."""
+        return str(value)
+
+    def get_int(self, value: bytes) -> int:
+        """Return the integer version of a btye."""
+        return int(value)
