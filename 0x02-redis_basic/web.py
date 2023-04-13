d43d00e26fdca1094c2141a56e6a0e6a96e268fa
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-13 02:06:23 +0000

    Seperated the caching of website content and tracking of url

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,21 +8,30 @@ from typing import Callable
 cache = redis.Redis()
 
 
-def store_cache(fn: Callable) -> Callable:
+def store_cache(method: Callable) -> Callable:
     """Cache a value for 10 seconds."""
-    @wraps(fn)
+    @wraps(method)
     def wrapper(url):
         """The function wrapper."""
-        cache.incr(f'count:{url}')
         if cache.get(url):
             return cache.get(url).decode()
-        content = fn(url)
+        content = method(url)
         cache.setex(url, 10, content)
         return content
     return wrapper
 
+def track_url(method: Callable) -> Callable:
+    """Cache a value for 10 seconds."""
+    @wraps(method)
+    def wrapper(url):
+        """The function wrapper."""
+        cache.incr(f'count:{url}')
+        return method(url)
+    return wrapper
+
 
 @store_cache
+@track_url
 def get_page(url: str) -> str:
     """Get the content of a webpage."""
     if cache.get(url):
